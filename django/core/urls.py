5d56a0e24df9e362ed5873e34f9cfae27c22991e
Traiano Welcome <traiano@gmail.com>
2019-03-24 16:41:29 +0800

    add detail view

--- django/core/urls.py
+++ django/core/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'core'
+
 urlpatterns = [
 
     path(
@@ -9,6 +11,7 @@ urlpatterns = [
         views.MovieList.as_view(),
         name='MovieList'
         ),
+
     path(
         'movie/<int:pk>',
         views.MovieDetail.as_view(),
